eb4a82ff2038bc95dc3f2ccf112ee43db98af196
Maria Granados <mariafgrana2@gmail.com>
2024-09-10 12:28:53 -0600

    feat: function que maneja el input del nombre

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 console.log("Conteo de vocales");
+
+/**
+ Encuentra el número de letras que eixten en el
+ nombre
+ @letra {string} letra a buscar
+ @nombre {string} nombre a buscar la letra
+ return número de letras encontradas
+*/
+function encontrarNumeroDeLetra( letra, nombre ) {    
+    let numeroDeLetras = 0;
+    for(let index = 0; index < nombre.length; index++ ) {
+        let letraAEvaluar = nombre.charAt( index );
+        if( letraAEvaluar === letra){
+            numeroDeLetras++;
+        }
+    }
+    return numeroDeLetras;
+}
+
+console.log(encontrarNumeroDeLetra("a","estefany"));
+
+function manejoDelEventoDeInputNombre(){
+   console.log("Me estoy invocando");
+}
